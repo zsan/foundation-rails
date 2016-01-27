ed4e9f561d1b20798dc7b0c97564b202e1ec02d0
Mehdi Lahmam <mehdi@craftsmen.io>
2016-01-27 05:56:19 +0100

    Use `bundle exec` when running commands from specs

--- spec/support/helpers.rb
+++ spec/support/helpers.rb
@@ -1,7 +1,7 @@
 module FoundationRailsTestHelpers
   def create_dummy_app
     FileUtils.cd(tmp_path) do
-      %x(rails new dummy --skip-active-record --skip-test-unit --skip-spring --skip-bundle)
+      %x(bundle exec rails new dummy --skip-active-record --skip-test-unit --skip-spring --skip-bundle)
       File.open(dummy_app_path + '/Gemfile', 'a') do |f|
         f.puts "gem 'foundation-rails', path: '#{File.join(File.dirname(__FILE__), '..', '..')}'"
       end
@@ -17,7 +17,7 @@ module FoundationRailsTestHelpers
 
   def install_foundation
     FileUtils.cd(dummy_app_path) do
-      %x(rails g foundation:install -f 2>&1)
+      %x(bundle exec rails g foundation:install -f 2>&1)
     end
   end
 
